d9ca2103bcc8482277803524d77e4f9ec1c50e5a
David Chelberg <chelberg@ohio.edu>
2020-01-30 09:23:00 -0500

    Initial commit of .cc code

---
+++ examples/lec06/lec6-1.cc
@@ -0,0 +1,18 @@
+// Example code to demonstrate built-in functions
+//
+// Written by David M. Chelberg
+// last-modified: Tue Jan 29 11:22:05 2013
+// 
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main( )
+{
+  double length_side = sqrt(9.0);
+  cout << "The square root of 9.0 is:" 
+       << length_side << endl;
+
+  return(EXIT_SUCCESS);
+}
